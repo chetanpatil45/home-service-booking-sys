39d819eabe4367654a692c439926b5685c1456a9
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-05-29 14:02:02 +0530

    Initial commit

---
+++ src/main/java/com/homeservicebooking/HomeServiceBookingApplication.java
@@ -0,0 +1,13 @@
+package com.homeservicebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServiceBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServiceBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homeservicebooking/HomeServiceBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeservicebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeServiceBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
